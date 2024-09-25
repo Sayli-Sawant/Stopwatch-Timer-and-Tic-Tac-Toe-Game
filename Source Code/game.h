b945d7bbe30b693e866e49a108883aea93e00e19
Sayli-Sawant <65643291+Sayli-Sawant@users.noreply.github.com>
2024-09-26 01:01:26 +0530

    Add files via upload

---
+++ Source Code/game.h
@@ -0,0 +1,12 @@
+
+#ifndef GAME_H_
+#define GAME_H_
+
+
+int check_win(char player);
+void reset_game();
+
+
+
+
+#endif /* GAME_H_ */
---
+++ Source Code/lcd.h
@@ -0,0 +1,11 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+void lcd_config();
+void lcd_init();
+void lcd_print(char []);
+
+void lcd_command(char);
+void lcd_delay(int);
+
+#endif /* LCD_H_ */
